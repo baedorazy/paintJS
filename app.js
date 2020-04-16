19262cc82ff6a696b4b36f089ad66f51718aab2b
jina bae <baedorazy@gmail.com>
2020-04-17 00:21:46 +0900

    .

--- app.js
+++ app.js
@@ -10,17 +10,25 @@ canvas.height= 600;
 ctx.strokeStyle = "#222";
 ctx.lineWidth = "2.5";
 
+//color control
+const colors = document.getElementsByClassName("controls__color");
+//colors = Array.from(colors);
+colors.forEach(color => color.addEventListener("click", changeColor));
+
+function changeColor(event) {
+    console.log(event);
+}
+
 function startPainting() {
     painting = true;
-    
-
 }
+
 function stopPainting() {
     painting = false;
 }
 
 function onMouseMove(event) {
-    //전체 윈도우의 좌표 clientX, clientY
+    // 전체 윈도우의 좌표 clientX, clientY
     // 캔버스 안에서의 가로세로 값은 offsetX, Y
     const x = event.offsetX;
     const y = event.offsetY;
